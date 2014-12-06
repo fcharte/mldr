96ae19c2421b5bfc55f0b57b06ad07d19d3de13d
Francisco Charte Ojeda <francisco@fcharte.com>
2014-12-06 10:32:03 +0100

    List of labelsets sorted by frequency

--- R/mldr.R
+++ R/mldr.R
@@ -90,7 +90,7 @@ mldr <- function(filename = NULL,
 
     obj$labels <- label_measures(obj$dataset, labeli)
 
-    obj$labelsets <-   table(as.factor(do.call(paste, c(obj$dataset[, obj$labels$index], sep = ""))))
+    obj$labelsets <-   sort(table(as.factor(do.call(paste, c(obj$dataset[, obj$labels$index], sep = "")))))
 
     obj$dataset <- dataset_measures(obj)
 
