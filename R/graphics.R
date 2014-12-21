f029376f011324f465c1853bb00255d6b572eac8
Francisco Charte Ojeda <francisco@fcharte.com>
2014-12-21 15:14:11 +0100

    Improved co-ocurrence plot
    Adjustments in titles and axis

--- R/graphics.R
+++ R/graphics.R
@@ -30,27 +30,31 @@ labelCoocurrencePlot <- function(mld) {
   colnames(tbl) <- colnames(labels)
   row.names(tbl) <- colnames(tbl)
 
-  circos.par(gap.degree = 3)
+  circos.par(gap.degree = 2)
   chordDiagram(tbl, annotationTrack = "grid", transparency = 0.5,
-               preAllocateTracks = list(track.height = 0.1))
+               preAllocateTracks = list(track.height = 0.2))
   for(si in get.all.sector.index()) {
-    circos.axis(h = "top", labels.cex = 0.4, sector.index = si, track.index = 2)
+    circos.axis(h = "top", labels.cex = 0.4, sector.index = si, track.index = 2, direction = "inside", labels = FALSE)
   }
   circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
     xlim = get.cell.meta.data("xlim")
     ylim = get.cell.meta.data("ylim")
+    xr <- get.cell.meta.data("xrange")
     sector.name <- get.cell.meta.data("sector.index")
     name.length <- nchar(sector.name)
     first.part <- if(name.length <= 3) name.length else 3
     second.part <- name.length - 3
     title <- paste(substr(sector.name, 1, first.part), substr(sector.name, second.part, name.length), sep = "-")
 
-    circos.lines(xlim, c(mean(ylim), mean(ylim)), lty = 3)
-    for(p in seq(0, 1, by = 0.25)) {
-      circos.text(p*(xlim[2] - xlim[1]) + xlim[1], mean(ylim), p, cex = 0.5, adj = c(0.5, -0.2), niceFacing = TRUE)
-    }
+    circos.lines(xlim, c(mean(ylim) - 0.4, mean(ylim) - 0.4), lty = 3)
+    if(xr > 15)
+      for(p in seq(0, 1, by = 0.25)) {
+        circos.text(p*(xlim[2] - xlim[1]) + xlim[1],
+                    mean(ylim) - 0.4, p, cex = 0.5, adj = c(0.5, -0.2), niceFacing = TRUE)
+      }
 
-    circos.text(mean(xlim), 1.1, title, cex = 0.6, niceFacing = TRUE)
+    circos.text(mean(xlim), 0, xr, cex = 0.7, niceFacing = TRUE)
+    circos.text(mean(xlim), 0.8, title, cex = 0.7, niceFacing = TRUE, facing = "clockwise")
   }, bg.border = NA)
   circos.clear()
 
