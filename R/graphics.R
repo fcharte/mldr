5114f5e6492e93e4b8f130be7c6f4b968cbd00b6
Francisco Charte Ojeda <francisco@fcharte.com>
2014-12-21 13:04:44 +0100

    Fixed title size in co-ocurrence plot

--- R/graphics.R
+++ R/graphics.R
@@ -34,18 +34,23 @@ labelCoocurrencePlot <- function(mld) {
   chordDiagram(tbl, annotationTrack = "grid", transparency = 0.5,
                preAllocateTracks = list(track.height = 0.1))
   for(si in get.all.sector.index()) {
-    circos.axis(h = "top", labels.cex = 0.6, sector.index = si, track.index = 2)
+    circos.axis(h = "top", labels.cex = 0.4, sector.index = si, track.index = 2)
   }
   circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
     xlim = get.cell.meta.data("xlim")
     ylim = get.cell.meta.data("ylim")
-    sector.name = get.cell.meta.data("sector.index")
+    sector.name <- get.cell.meta.data("sector.index")
+    name.length <- nchar(sector.name)
+    first.part <- if(name.length <= 3) name.length else 3
+    second.part <- name.length - 3
+    title <- paste(substr(sector.name, 1, first.part), substr(sector.name, second.part, name.length), sep = "-")
 
     circos.lines(xlim, c(mean(ylim), mean(ylim)), lty = 3)
     for(p in seq(0, 1, by = 0.25)) {
-      circos.text(p*(xlim[2] - xlim[1]) + xlim[1], mean(ylim), p, cex = 0.8, adj = c(0.5, -0.2), niceFacing = TRUE)
+      circos.text(p*(xlim[2] - xlim[1]) + xlim[1], mean(ylim), p, cex = 0.5, adj = c(0.5, -0.2), niceFacing = TRUE)
     }
-    circos.text(mean(xlim), 1.4, sector.name, niceFacing = TRUE)
+
+    circos.text(mean(xlim), 1.1, title, cex = 0.6, niceFacing = TRUE)
   }, bg.border = NA)
   circos.clear()
 
