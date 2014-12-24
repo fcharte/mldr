50004bd0c5febd3a78c5881ee1c35e54ed32e95c
Francisco Charte Ojeda <francisco@fcharte.com>
2014-12-24 08:49:32 +0100

    File upload server side

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -3,11 +3,34 @@ library(mldr)
 
 shinyServer(function(input, output, session) {
   observe({
+    if(input$loadButton != 0) {
+      arfffile <- input$arffname
+      xmlfile <- input$xmlname
+      if(!is.null(arfffile) && !is.null(xmlfile)) {
+        .GlobalEnv[[arfffile$name]] <- mldr(arfffile$datapath, auto_extension = FALSE, xml_file = xmlfile$datapath)
+        updateSelectInput(session, "mldrs",
+                          choices = availableMLDs,
+                          selected = arfffile$name)
+      }
+    }
+
     availableMLDs <- as.list(ls(.GlobalEnv)[unlist(sapply(ls(.GlobalEnv), function(obj) class(get(obj)) == "mldr"))])
 
     updateSelectInput(session, "mldrs",
                       choices = availableMLDs,
                       selected = availableMLDs[[1]])
+  })
+
+  loader <- reactive({
+    print(input$loadButton)
+    if(input$loadButton != 0) {
+      print("loadButton")
+      arfffile <- input$arffname
+      xmlfile <- input$xmlname
+      if(!is.null(arffile) && !is.null(xmlfile)) {
+        print(paste(arfffile$datapath, xmlfile$datapath))
+      }
+    }
   })
 
   summaryTable <- reactive({
