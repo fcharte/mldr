aafe7e9f2d14035bff8bed51b60bd156b972f7f2
Francisco Charte Ojeda <francisco@fcharte.com>
2015-01-29 18:18:50 +0100

    Show the mldrGUI() message only if in interactive mode

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,4 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("enter mldrGUI() to launch mldr's web-based GUI")
-}
-
-.onLoad <- function(libname, pkgname) {
-  options(shiny.error=FALSE)
-  options(shiny.try = FALSE)
+.onAttach <- function(...) {
+  if(interactive())
+    packageStartupMessage("Enter mldrGUI() to launch mldr's web-based GUI")
 }
