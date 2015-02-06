7470ee8516d1e128ea89cbd3916c604777793bae
Francisco Charte Ojeda <francisco@fcharte.com>
2015-02-06 08:49:54 +0100

    Documented generic print and summary functions

--- R/generics.R
+++ R/generics.R
@@ -1,11 +1,32 @@
+#' @title Provides a summary of measures about the mldr
+#' @description Prints a summary of the measures obtained from the \code{mldr} object
+#' @param mld Object whose measures are to be printed
+#' @seealso \code{\link{mldr}}, \code{\link{print.mldr}}
+#' @examples
+#'
+#' library(mldr)
+#'
+#' summary(emotions)
+#'
 #' @export
 summary.mldr <- function(mld) {
   print(data.frame(mld$measures))
 }
 
+#' @title Prints the mldr content
+#' @description Prints the \code{mldr} object data, including input attributs and output labels
+#' @param mld Object whose data are to be printed
+#' @seealso \code{\link{mldr}}, \code{\link{summary.mldr}}
+#' @examples
+#'
+#' library(mldr)
+#'
+#' emotions
+#' print(emotions) # Same as above
+#'
 #' @export
 print.mldr <- function(mld) {
-  print(mld$dataset[,1:(ncol(mld$dataset)-2)])
+  print(mld$dataset[ , c(mld$attributesIndexes, mld$labels$index)])
 }
 
 
