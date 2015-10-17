b0fd05c424261ec3bc401b2614835847cd8c2370
Francisco Charte Ojeda <francisco@fcharte.com>
2015-10-17 20:56:08 +0200

    Fixed bug in code for rebuilding datasets and sample datasets updated

--- demo/rebuild_datasets.R
+++ demo/rebuild_datasets.R
@@ -1,6 +1,6 @@
-emotions <- mldr_from_dataframe(mldr::emotions$dataset, mldr::emotions$labels$index, "emotions")
-birds <- mldr_from_dataframe(mldr::birds$dataset, mldr::birds$labels$index, "birds")
-genbase <- mldr_from_dataframe(mldr::genbase$dataset, mldr::genbase$labels$index, "genbase")
+emotions <- mldr_from_dataframe(mldr::emotions$dataset[, c(emotions$attributesIndexes, emotions$labels$index)], mldr::emotions$labels$index, "emotions")
+birds <- mldr_from_dataframe(mldr::birds$dataset[, c(birds$attributesIndexes, birds$labels$index)], mldr::birds$labels$index, "birds")
+genbase <- mldr_from_dataframe(mldr::genbase$dataset[, c(genbase$attributesIndexes, genbase$labels$index)], mldr::genbase$labels$index, "genbase")
 
 save(emotions, file = "emotions.rda", compress = "xz")
 save(birds, file = "birds.rda", compress = "xz")
