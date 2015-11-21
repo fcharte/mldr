c67245d082e39603627b26965dc4f6bae61ae6e9
David Charte <fdavidcl@outlook.com>
2015-11-21 19:09:24 +0100

    Improve correctness of Scumble CV calculation
    Return NA when there's one instance of the label or none

--- R/measures.R
+++ R/measures.R
@@ -88,9 +88,12 @@ dataset_measures <- function(mld) {
     # If the mean SCUMBLE of a label is 0, then its standard deviation is 0 (since SCUMBLE is always nonnegative),
     # thus we define its CV as 0.
     ####################################
-    mld$labels$SCUMBLE.CV <- ifelse(mld$labels$SCUMBLE == 0 | colSums(mld$dataset[mld$labels$index]) == 0,
-      0,
-      sapply(mld$labels$index, function(i) sd(mld$dataset[mld$dataset[, i] != 0, ".SCUMBLE"])) / mld$labels$SCUMBLE
+    mld$labels$SCUMBLE.CV <- ifelse(colSums(mld$dataset[mld$labels$index]) == 0,
+      NA,
+      ifelse(mld$labels$SCUMBLE == 0 | colSums(mld$dataset[mld$labels$index]) == 0,
+        0,
+        sapply(mld$labels$index, function(i) sd(mld$dataset[mld$dataset[, i] != 0, ".SCUMBLE"])) / mld$labels$SCUMBLE
+      )
     )
   }
   else {
